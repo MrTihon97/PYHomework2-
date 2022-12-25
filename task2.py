16fbb459e52f20033dcfbc0f1285ac65c42d08c0
MrTihon <tihonsomov@mail.ru>
2022-12-25 18:35:37 +0300

    tak 2

--- task2.py
+++ task2.py
@@ -5,12 +5,21 @@
 
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+# from random import randint
+
+# arr =[randint(0,10) for i in range(randint(3,5))]
+# sum = []
+# middle = len(arr)//2 + len(arr)%2
+# for i in range(middle):
+#         sum.append(arr[i] * arr[len(arr) - i - 1])
+# print(arr, '=>', (sum))
 
 
 arr = [2, 3, 4, 5, 6]
 sum = []
-for i in range(0, len(arr), 2):
-        sum = arr[i] * arr[len(arr) - i - 1]
+middle = len(arr)//2 + len(arr)%2
+for i in range(middle):
+        sum.append(arr[i] * arr[len(arr) - i - 1])
 print(arr, '=>', (sum))
 
 
